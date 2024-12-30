d7118b1f3c9befd2d6de4cdfa036387b63414753
JarvisStark24 <dass96852@gmail.com>
2024-12-30 13:43:28 +0530

    Backend integrated for user & captain signup/login/logout funtionalities with protected routes

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -1,24 +1,27 @@
-/* eslint-disable react-refresh/only-export-components */
-import { createContext, useState } from "react";
+import { createContext, useState } from 'react'
 
-export const UserDataContext = createContext();
+// eslint-disable-next-line react-refresh/only-export-components
+export const UserDataContext = createContext()
+
 
 // eslint-disable-next-line react/prop-types
 const UserContext = ({ children }) => {
-  const [user, setUser] = useState({
-    email: "",
-    fullName: {
-      firstName: "",
-      lastName: "",
-    },
-  });
-  return (
-    <div>
-      <UserDataContext.Provider value={[user, setUser]}>
-        {children}
-      </UserDataContext.Provider>
-    </div>
-  );
-};
 
-export default UserContext;
+    const [ user, setUser ] = useState({
+        email: '',
+        fullName: {
+            firstName: '',
+            lastName: ''
+        }
+    })
+
+    return (
+        <div>
+            <UserDataContext.Provider value={{ user, setUser }}>
+                {children}
+            </UserDataContext.Provider>
+        </div>
+    )
+}
+
+export default UserContext
---
+++ frontend/src/pages/CaptainHome.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const CaptainHome = () => {
+  return (
+    <div>
+      Captain Home
+    </div>
+  )
+}
+
+export default CaptainHome
