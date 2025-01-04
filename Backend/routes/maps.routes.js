fa3fbfd79a3fe676ceef2efb55157fa0e830c5f4
JarvisStark24 <dass96852@gmail.com>
2025-01-04 12:18:58 +0530

    address testing (lng,ltd) done with postman

--- Backend/routes/maps.routes.js
+++ Backend/routes/maps.routes.js
@@ -1,8 +1,11 @@
 const express = require ('express')
 const router = express.Router()
 const authMiddleware = require('../middlewares/auth.middleware');
-const mapController = require('../controllers/maps.controller')
+const mapController = require('../controllers/map.controller')
+const {query} = require('express-validator')
 
-router.get('/get-coordinates', authMiddleware.authUser, getCoordinate)
+router.get('/get-coordinates', 
+    query('address').isString().isLength({min: 3}),
+    authMiddleware.authUser, mapController.getCoordinates)
 
 module.exports = router
