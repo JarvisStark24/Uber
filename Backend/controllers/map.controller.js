e344d10d8c03b19dd5533a10c1f9d86144ec3aa4
JarvisStark24 <dass96852@gmail.com>
2025-01-04 12:54:36 +0530

    map testings done with address, distance & time, auto-suggestions

--- Backend/controllers/map.controller.js
+++ Backend/controllers/map.controller.js
@@ -2,11 +2,10 @@ const mapService = require("../services/maps.service");
 const { validationResult } = require("express-validator");
 
 module.exports.getCoordinates = async (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-    }
-
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
 
   const { address } = req.query;
   try {
@@ -14,5 +13,42 @@ module.exports.getCoordinates = async (req, res, next) => {
     res.status(200).json(coordinates);
   } catch (error) {
     res.status(404).json({ message: "Coordinates not found" });
+  }
+};
+
+module.exports.getDistanceTime = async (req, res, next) => {
+
+  try {
+
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+          return res.status(400).json({ errors: errors.array() });
+      }
+
+      const { origin, destination } = req.query;
+
+      const distanceTime = await mapService.getDistanceTime(origin, destination);
+
+      res.status(200).json(distanceTime);
+
+  } catch (err) {
+      console.error(err);
+      res.status(404).json({ message: 'Distance and Time not found' });
+  }
+}
+
+module.exports.getAutoCompleteSugestions = async (req, res, next) => {
+  try {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { input } = req.query;
+  
+    const sugestions = await mapService.getAutoCompleteSugestions(input);
+    res.status(200).json(sugestions);
+  } catch (error) {
+    res.status(404).json({ message: "Sugestions not found" });
   }
 };
--- Backend/routes/maps.routes.js
+++ Backend/routes/maps.routes.js
@@ -8,4 +8,16 @@ router.get('/get-coordinates',
     query('address').isString().isLength({min: 3}),
     authMiddleware.authUser, mapController.getCoordinates)
 
+    router.get('/get-distance-time',
+        query('origin').isString().isLength({ min: 3 }),
+        query('destination').isString().isLength({ min: 3 }),
+        authMiddleware.authUser,
+        mapController.getDistanceTime
+    )
+
+router.get('/get-sugestions', 
+    query('input').isString().isLength({ min: 3 }),
+    authMiddleware.authUser,
+    mapController.getAutoCompleteSugestions)
+
 module.exports = router
